74c4ec39700015573f133eb7b5b3f7a966b90968
Shiva Shekhar Sukuri <shivashekarsukuri2000@gmail.com>
2025-01-30 20:39:38 +0530

    ep-1

---
+++ app.js
@@ -0,0 +1,6 @@
+// const heading=React.createElement('h1',{id:'heading'},'Hello World from React')
+const root=ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+const parent=React.createElement('div',{id:'parent'},[React.createElement('div',{id:'child'},[React.createElement('h1',{},'iam h1 tag'),React.createElement('h2',{},'iam h2 tag')]),React.createElement('div',{id:'child2'},[React.createElement('h1',{},'iam h1 tag'),React.createElement('h2',{},'iam h2 tag')])])
+root.render(parent)
