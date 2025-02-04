8f58e9d9d1f527b3293e5fc416e0502286311e3c
Shiva Shekhar Sukuri <shivashekarsukuri2000@gmail.com>
2025-02-04 20:32:56 +0530

    ep-4

--- app.js
+++ app.js
@@ -1,35 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import srLogo from './sr-logo.jpeg'
 
-// // const heading=React.createElement('h1',{id:'heading'},'Hello World from React')
-const root=ReactDOM.createRoot(document.getElementById('root'))
-// // // root.render(heading)
-    
-// // const parent=React.createElement('div',{id:'parent'},[React.createElement('div',{id:'child'},[React.createElement('h1',{},'iam h1 tag'),React.createElement('h2',{},'iam h2 tag')]),React.createElement('div',{id:'child2'},[React.createElement('h1',{},'iam h1 tag'),React.createElement('h2',{},'iam h2 tag')])])
-// const Title=()=><h1 className='heading'>Hello world from react</h1>
-// const head=<h2>Iam React Elelemnt including in jsx</h2>
-// console.log(head)
-
-// const HeadingComponent=()=>{
-//     return <div>
-//         {head}
-//         <Title></Title>
-//         <h1>iam functional comoponent </h1>\
-//         </div>
-// }
-// root.render(<HeadingComponent />)
-
-//assignment ep-2
-
-const HeaderComponent=()=>{
+const Header=()=>{
     return (
-        <div id="header">
-            <img src={srLogo} id='logo'/>
-            <input type="text" id='search'></input>
-            <span id='user'><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></span>
+        <div className='header'>
+            <div className='logo-container'>
+                <img alt='app-logo' className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All'></img>
+            </div>
+            <div className='nav-container'>
+                <ul>
+                    <li>Home</li>
+                    <li>Cart</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
         </div>
     )
 }
 
-root.render(<HeaderComponent/>)
+const RestoCard=()=>{
+  return (
+    <div className='resto-card'>
+        <img className='res-logo' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597'></img>
+                <h2>Pizza Hut</h2>
+                <h5>Chinese,Asian,Tibetian,Desserts</h5>
+                <h5>4.2</h5>
+                <h5>35 Minutes</h5>
+    </div>
+  )
+}
+
+const Body=()=>{
+    return (
+        <div className='body'>
+            <div className='search'>
+
+            </div>
+            <div className='card-container'>
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            <RestoCard />
+            </div>
+        </div>
+    )
+}
+
+
+
+const AppComponent=()=>{
+    return (
+        <div className='app'>
+            <Header />
+            <Body></Body>
+        </div>
+    )
+}
+
+
+const root =ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppComponent />)
